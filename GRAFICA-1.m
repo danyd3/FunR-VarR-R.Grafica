dcd3840f1fc3f791e1e1429a0433c8654d7bf426
danyd3 <89928594+danyd3@users.noreply.github.com>
2021-10-28 19:50:26 -0500

    Add files via upload

---
+++ GRAFICA-1.m
@@ -0,0 +1,17 @@
+%Octave Script
+%Title              :Funcion real de variable real
+%Descrption         :Graficación de una función real de variable real #2
+%Author             :Daniel Alberto De Jesús (danyd3)
+%Date               :202123231
+%Version            :1
+%Usage              :octave> /path/GRAFICA1
+%Notes              :Requiere aplicación octave junto a linea de comandos.
+
+clear
+% Dominio de la función
+x=-0:0.1:50
+% Regla de correspondecia (función)
+%1+?x-4
+fx=(1)+((x-4).^(1/2))
+% Plotear función
+plot(x, fx);
